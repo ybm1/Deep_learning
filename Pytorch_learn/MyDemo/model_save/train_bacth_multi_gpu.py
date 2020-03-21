763a312fb2711eb2fce952bb9d9c2a47e30b6df9
mayanbin <1198126894@qq.com>
2020-03-21 19:25:28 +0800

    多GPU训练

---
+++ Pytorch_learn/MyDemo/model_save/train_bacth_multi_gpu.py
@@ -0,0 +1,133 @@
+from __future__ import print_function
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from model import Net
+from multi_input_data import get_data,Mydataset
+from torch.utils.data import DataLoader
+import Const as C
+
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter(C.PATH_to_log_dir)
+
+## 使用
+# tensorboard --logdir=/Users/biqixuan/PycharmProjects/Deep_learning/Pytorch_learn/MyDemo/logs
+
+# 进行tensorboard的查看
+
+
+def mytrain(model, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, sample_batched in enumerate(train_loader):
+        p1 = sample_batched["features"]["p1"]
+        p2 = sample_batched["features"]["p2"]
+        p3 = sample_batched["features"]["p3"]
+
+        target = sample_batched["label"]
+
+        #p1,p2,p3,target = p1.to(device),p2.to(device),p3.to(device),target.to(device)
+        p1, p2, p3, target = p1.cuda(), p2.cuda(), p3.cuda(), target.cuda()
+
+        optimizer.zero_grad()
+        output = model(p1,p2,p3)
+
+        loss = F.mse_loss(output, target)
+
+        loss.backward()
+
+        optimizer.step()
+        bias = F.l1_loss(output, target)
+
+        writer.add_scalar('training MSE',
+                          loss.item(),
+                          epoch * len(train_loader) + batch_idx)
+        writer.add_scalar('training l1 loss',
+                          bias.item(),
+                          epoch * len(train_loader) + batch_idx)
+
+
+        if batch_idx % C.LOG_INTERVAL == 0:
+            print('Train Epoch: {} Batch_idx :{} \tLoss: {:.6f}'.format(
+                epoch, batch_idx , loss.item()))
+
+
+def mytest(model, test_loader,epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data in test_loader:
+            p1 = data["features"]["p1"]
+            p2 = data["features"]["p2"]
+            p3 = data["features"]["p3"]
+
+            target = data["label"]
+
+            #p1, p2, p3, target = p1.to(device), p2.to(device), p3.to(device), target.to(device)
+            p1, p2, p3, target = p1.cuda(), p2.cuda(), p3.cuda(), target.cuda()
+
+            output = model(p1,p2,p3)
+            test_loss += F.mse_loss(input = output, target = target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    writer.add_scalar('Epoch test MSE',
+                      test_loss,epoch)
+
+    print('\nTest set:Epoch: {}  Average MSE loss: {:.4f}'.format(epoch,test_loss))
+
+
+
+
+if __name__ == '__main__':
+    import os
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    train_input = Mydataset(get_data(C.TRAIN_DATA_SIZE))
+    test_input = Mydataset(get_data(C.TEST_DATA_SIZE))
+
+    train_loader = DataLoader(train_input,
+                              batch_size=C.BATCH_SIZE,
+                              shuffle=True, num_workers=2)
+
+    test_loader = DataLoader(test_input,
+                             batch_size=C.BATCH_SIZE,
+                             shuffle=True, num_workers=2)
+
+    device_ids = [0, 1, 2, 3]
+    model = Net()
+    model = torch.nn.DataParallel(model, device_ids=device_ids)
+    model = model.cuda(device_ids[0])
+
+
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    #print("device===>",device)
+
+    optimizer = optim.Adadelta(model.parameters(), lr=C.LEARNING_RATE)
+    optimizer = torch.nn.DataParallel(optimizer, device_ids=device_ids)\
+
+    scheduler = StepLR(optimizer, step_size=2, gamma=C.gamma)
+
+    for epoch in range(1, C.EPOCHS + 1):
+        if C.RESTORE_MODEL:
+            print("模型开始增量训练==>>")
+            model = torch.load(C.MODEL_SAVE_PATH)
+            mytrain(model,  train_loader, optimizer, epoch)
+            mytest(model, test_loader,epoch)
+            scheduler.step()
+
+        else:
+            mytrain(model,  train_loader, optimizer, epoch)
+            mytest(model, test_loader, epoch)
+            scheduler.step()
+
+
+    if C.SAVE_MODEL:
+        torch.save(model, C.MODEL_SAVE_PATH)
+
+
+## 参考了：https://github.com/pytorch/examples/blob/master/mnist/main.py
